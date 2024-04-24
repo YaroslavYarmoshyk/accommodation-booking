d173d4d6ad9cbdaa30e8fca209cde514d2cff7d2
Yaroslav <yarmoshyk96@gmail.com>
2024-04-24 11:09:13 +0300

    First commit

---
+++ src/test/java/com/accommodationbooking/AccommodationBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accommodationbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
