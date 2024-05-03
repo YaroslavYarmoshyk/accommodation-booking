30b9b88fb3e08ebe88eab59ff3828fdcc72dfba4
Yaroslav <yarmoshyk96@gmail.com>
2024-05-03 21:40:45 +0300

    Adjust base gradle set up

--- src/test/java/com/accommodationbooking/AccommodationBookingApplicationTests.java
+++ src/test/java/com/accommodationbooking/AccommodationBookingApplicationTests.java
@@ -1,9 +1,15 @@
 package com.accommodationbooking;
 
+import com.accommodationbooking.config.TestContainerConfiguration;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+
+@Import(TestContainerConfiguration.class)
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class AccommodationBookingApplicationTests {
 
 	@Test
